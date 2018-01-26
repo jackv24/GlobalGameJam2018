0611e217dbd03aeec702faa89f7bfbf257dbad80
Unknown <jake-perry@live.com>
2018-01-27 02:40:47 +1030

    Enemy alerts
    ShipControls now raises an event when pinging for resources.
    GameManager listens for this event and alerts all other ships of the sender's position when it occurs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,7 +41,10 @@ public class GameManager : MonoBehaviour
 
             ShipControls ship = playerObj.GetComponentInChildren<ShipControls>();
             if (ship)
+            {
                 playerShips.Add(ship);
+                ship.OnTransmissionPing += OnShipPing;
+            }
 
             cameraObj.transform.SetParent(playerObj.transform);
 
@@ -79,6 +82,18 @@ public class GameManager : MonoBehaviour
                 {
                     hud.worldCamera = cam;
                 }
+            }
+        }
+    }
+
+    private void OnShipPing(ShipControls sender)
+    {
+        // Alert all other ships to sender ship's location
+        foreach (var ship in playerShips)
+        {
+            if (ship != sender)
+            {
+                ship.AlertEnemyPresence(sender);
             }
         }
     }
