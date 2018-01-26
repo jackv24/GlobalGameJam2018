dc29111ee505d25715a90755ac25e98019f93d0a
Jack Vine <jackv949@gmail.com>
2018-01-26 23:31:01 +1030

    Added nested prefabs

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            Vector3 pos = transform.position;
+
+            pos.x = target.position.x;
+            pos.y = target.position.y;
+
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/SplitScreenSetup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SplitScreenSetup : MonoBehaviour
+{
+    public int screenPos = 1;
+    public int maxScreens = 4;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    public void Setup()
+    {
+        Rect rect = cam.rect;
+
+        if(maxScreens == 4)
+        {
+            switch(screenPos)
+            {
+                case 1:
+                    rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+                    break;
+                case 2:
+                    rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                    break;
+                case 3:
+                    rect = new Rect(0, 0, 0.5f, 0.5f);
+                    break;
+                case 4:
+                    rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                    break;
+            }
+        }
+        else if (maxScreens == 3)
+        {
+            switch (screenPos)
+            {
+                case 1:
+                    rect = new Rect(0, 0, 0.5f, 1.0f);
+                    break;
+                case 2:
+                    rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                    break;
+                case 3:
+                    rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                    break;
+            }
+        }
+        else if (maxScreens == 2)
+        {
+            switch (screenPos)
+            {
+                case 1:
+                    rect = new Rect(0, 0, 0.5f, 1.0f);
+                    break;
+                case 2:
+                    rect = new Rect(0.5f, 0, 0.5f, 1.0f);
+                    break;
+            }
+        }
+
+        cam.rect = rect;
+    }
+}
