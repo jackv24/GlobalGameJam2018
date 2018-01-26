f231e591c226e8edeb8d62ea886284ea6f2ee9d7
Jack Vine <jackv949@gmail.com>
2018-01-26 21:55:33 +1030

    Platformer improvements, setup layers

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -3,25 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlatformerController : Controllable
 {
+    public float inputDeadZone = 0.2f;
     public float moveSpeed = 5.0f;
+
+    [Space()]
     public float jumpForce = 5.0f;
+    private bool shouldJump = false;
+
+    public float groundedDistance = 0.1f;
+    public LayerMask groundLayer;
+
+    private float inputDirection;
+    private float oldInputDirection = 1;
 
     private Rigidbody2D body;
+    private BoxCollider2D col;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        bool grounded = CheckGrounded();
+
+        Vector2 velocity = body.velocity;
+
+        velocity.x = moveSpeed * inputDirection;
+
+        if(shouldJump)
+        {
+            if(grounded && velocity.y <= 0)
+                velocity.y = jumpForce;
+
+            shouldJump = false;
+        }
+
+        body.velocity = velocity;
     }
 
     public override void Move(Vector2 inputVector)
     {
-        Vector2 velocity = body.velocity;
+        if(inputDirection != 0)
+            oldInputDirection = inputDirection;
 
-        velocity.x = inputVector.x * moveSpeed;
+        inputDirection = 0;
 
-        body.velocity = velocity;
+        if (Mathf.Abs(inputVector.x) >= inputDeadZone)
+            inputDirection = Mathf.Sign(inputVector.x);
+
+        if(inputDirection != 0 && inputDirection != oldInputDirection)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
     }
 
     public override void Jump(ButtonState buttonState)
@@ -29,11 +70,7 @@ public class PlatformerController : Controllable
         switch(buttonState)
         {
             case ButtonState.WasPressed:
-                Vector2 velocity = body.velocity;
-
-                velocity.y = jumpForce;
-
-                body.velocity = velocity;
+                shouldJump = true;
                 break;
         }
     }
@@ -41,5 +78,22 @@ public class PlatformerController : Controllable
     public override void Shoot(ButtonState buttonState)
     {
         
+    }
+
+    bool CheckGrounded()
+    {
+        bool grounded = false;
+
+        Vector2 origin = (Vector2)transform.position + col.offset;
+        float length = ((col.size.y * transform.localScale.y) / 2) + groundedDistance;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, length, groundLayer);
+
+        Debug.DrawLine(origin, origin + Vector2.down * length);
+
+        if (hit.collider != null)
+            grounded = true;
+
+        return grounded;
     }
 }
