5cc27299a2f5e10246cb5f8576d621a2948b6b84
Unknown <jake-perry@live.com>
2018-01-27 13:09:18 +1030

    Added Parallax Camera & Parallax Object
    Added parallax camera which moves ParallaxObject instances per camera in pre render.
    Projectiles no longer collide with each other.

---
+++ Assets/Scripts/Parallax/ParallaxObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxObject : MonoBehaviour
+{
+    [SerializeField]
+    private float distanceFromCamera;
+
+    public float DistanceFromCamera { get { return distanceFromCamera; } }
+
+    public Vector2 Offset { get; private set; }
+
+    private void Awake()
+    {
+        Offset = transform.position;
+    }
+}
