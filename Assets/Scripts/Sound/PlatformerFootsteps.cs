a59e8a75a7ab1a25610ac523e92e76ad1102cd4f
Jack Vine <jackv949@gmail.com>
2018-01-27 23:50:05 +1030

    no message

---
+++ Assets/Scripts/Sound/PlatformerFootsteps.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerFootsteps : MonoBehaviour
+{
+    public AudioEvent footstepSound;
+
+    public void PlayFootstep()
+    {
+        footstepSound.Play(transform.position);
+    }
+}
