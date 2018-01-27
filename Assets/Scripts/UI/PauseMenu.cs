d2dc5f6cc54b1d6ff3bb418833883f517b90ca9f
Jack Vine <jackv949@gmail.com>
2018-01-27 23:19:26 +1030

    Added pause menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using InControl;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(CanvasGroup))]
 public class PauseMenu : MonoBehaviour
 {
+    public static bool IsPaused { get { return instance.paused; } }
+
+    private static PauseMenu instance;
+
     public Button resumeButton;
     public Button menuButton;
 
@@ -17,6 +22,8 @@ public class PauseMenu : MonoBehaviour
 
     private void Awake()
     {
+        instance = this;
+
         group = GetComponent<CanvasGroup>();
     }
 
@@ -32,35 +39,58 @@ public class PauseMenu : MonoBehaviour
             menuButton.onClick.AddListener(delegate { LoadScene(); });
         }
 
-        if(group)
-        {
-            group.alpha = 0;
-            group.interactable = false;
-            group.blocksRaycasts = false;
-        }
+        UnPause();
     }
 
     private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape) || InputManager.ActiveDevice.Command.WasPressed)
+        {
+            if (paused)
+                UnPause();
+            else
+                Pause();
+        }
     }
 
     public void UnPause()
     {
-
-
+        Time.timeScale = 1.0f;
         paused = false;
+
+        if (group)
+        {
+            group.alpha = 0;
+            group.interactable = false;
+            group.blocksRaycasts = false;
+
+            EventSystem.current.firstSelectedGameObject = null;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 
     public void Pause()
     {
-
-
+        Time.timeScale = 0;
         paused = true;
+
+        if (group)
+        {
+            group.alpha = 1;
+            group.interactable = true;
+            group.blocksRaycasts = true;
+
+            GameObject firstButton = transform.GetComponentInChildren<Button>().gameObject;
+
+            EventSystem.current.firstSelectedGameObject = firstButton;
+            EventSystem.current.SetSelectedGameObject(firstButton);
+        }
     }
 
     public void LoadScene()
     {
+        UnPause();
+
         SceneManager.LoadScene(0);
     }
 }
