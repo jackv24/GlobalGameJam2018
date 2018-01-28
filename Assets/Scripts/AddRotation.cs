eb6e9d38fd43223dcb1b71972717e3f027f66d39
Unknown <jake-perry@live.com>
2018-01-28 15:26:01 +1030

    Sorting layers, asteroids in Game scene

---
+++ Assets/Scripts/AddRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRotation : MonoBehaviour
+{
+    public float amount;
+
+    private void Update()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.z += amount * Time.deltaTime;
+        transform.eulerAngles = rotation;
+    }
+}
